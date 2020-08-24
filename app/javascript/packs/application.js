a5a74f9ec952ff92389f1b56dae7dd9046d38eda
ToshiKuwako <toshiki.se@gmail.com>
2020-08-24 12:30:13 +0900

    AjaxAppの準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../checked")
+require("../memo")　
+// 　require("<該当ファイルをパスで指定>でファイルの読み込み")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
